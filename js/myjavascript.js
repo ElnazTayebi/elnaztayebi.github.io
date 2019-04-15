fa0ea9ba9d3ed14674c94139734e72b568b7cfce
ElnazTayebi <elnazt67@gmail.com>
2019-04-15 13:42:04 +0430

    add input mask

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -1,17 +1,20 @@
-var timer2 = "2:00";
-var interval = setInterval(function() {
+// var timer2 = "2:00";
+// var interval = setInterval(function() {
+//
+//
+//     var timer = timer2.split(':');
+//     //by parsing integer, I avoid all extra string processing
+//     var minutes = parseInt(timer[0], 10);
+//     var seconds = parseInt(timer[1], 10);
+//     --seconds;
+//     minutes = (seconds < 0) ? --minutes : minutes;
+//     if (minutes < 0) clearInterval(interval);
+//     seconds = (seconds < 0) ? 59 : seconds;
+//     seconds = (seconds < 10) ? '0' + seconds : seconds;
+//     //minutes = (minutes < 10) ?  minutes : minutes;
+//     $('.countdown').html(minutes + ':' + seconds);
+//     timer2 = minutes + ':' + seconds;
+// }, 1000);
 
 
-    var timer = timer2.split(':');
-    //by parsing integer, I avoid all extra string processing
-    var minutes = parseInt(timer[0], 10);
-    var seconds = parseInt(timer[1], 10);
-    --seconds;
-    minutes = (seconds < 0) ? --minutes : minutes;
-    if (minutes < 0) clearInterval(interval);
-    seconds = (seconds < 0) ? 59 : seconds;
-    seconds = (seconds < 10) ? '0' + seconds : seconds;
-    //minutes = (minutes < 10) ?  minutes : minutes;
-    $('.countdown').html(minutes + ':' + seconds);
-    timer2 = minutes + ':' + seconds;
-}, 1000);
+$(":input").inputmask();
